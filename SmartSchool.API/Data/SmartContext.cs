ed6095b3ea26d025f21fa5feb59dbe8c248cd6d8
Eliseu Oliveira <eliseu.dev@outlook.com>
2022-08-30 21:44:57 -0300

    Configurando appsettings e dbcontext

---
+++ SmartSchool.API/Data/SmartContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Models;
+
+namespace SmartSchool.API.Data
+{
+    public class SmartContext : DbContext
+    {
+        public SmartContext(DbContextOptions<SmartContext> options) : base(options) { }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<AlunoDisciplina> AlunosDisciplinas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AlunoDisciplina>()
+                .HasKey(ad => new { ad.AlunoId, ad.DisciplinaId });
+        }
+    }
+}
